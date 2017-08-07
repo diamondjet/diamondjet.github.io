3d44b1ef0d91ed193e1aada7e3fcbe367927457e
diamondjet <jetyzack@gmail.com>
2017-08-07 14:32:08 +1000

    Add files via upload

--- Projects/Solution/score/score.php
+++ Projects/Solution/score/score.php
@@ -5,8 +5,21 @@ switch ($function) {
     case 'addScore':
     $xmlDoc = new DOMDocument();
     $xmlDoc->load("score.xml");
-
-        break;
+    $x = $xmlDoc->documentElement;
+    $done = false;
+    foreach ($x->childNodes AS $item) {
+        if ($item->nodeName == $_POST['username']) {
+            $item->nodeValue = $item->nodeValue + 1;
+            //$log['b']=$xmlDoc;
+            $done = true;
+        }
+    }
+    if (!$done) {
+        $score = $xmlDoc->getElementsByTagName('score');
+        $x->appendChild('check','works');//NEED TO ADD NEW ELEMENT
+    }
+    $xmlDoc->save("score.xml");
+    break;
 
     default:
         # code...
